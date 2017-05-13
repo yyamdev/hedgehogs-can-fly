81ef615866c4608c595f13de56edd308c08b75da
yyam <yyamdev@gmail.com>
2017-05-14 00:15:48 +0100

    Removed old code from entity.

--- ballfling/entity.h
+++ ballfling/entity.h
@@ -7,8 +7,6 @@
 #include <string>
 #include "subject.h"
 
-#define ENTITY_DAMAGE_COOLDOWN_MILLISECONDS 1000
-
 class World;
 class EntityTerrain;
 
@@ -28,11 +26,6 @@ public:
 
     std::string get_tag();
 
-    int get_hp();
-    int get_max_hp();
-    void set_hp(int hp);
-    void damage(int dmg);
-
     sf::Vector2f position;
     sf::Vector2f velocity;
     float collisionRadius;
@@ -40,17 +33,7 @@ public:
 
     void set_world_ptr(World *world);
 protected:
-    // helper methods
-    static sf::Vector2f reflect(const sf::Vector2f incident, const sf::Vector2f unitNormal);
-    static sf::Vector2f apply_friction(const sf::Vector2f vector, const float friction);
-    static float dot(const sf::Vector2f a, const sf::Vector2f b);
-
     std::string tag;
     World *world;
-    int hp;
-    int maxHp;
-    bool invulnerable;
-
-    sf::Clock clockDamageCooldown;
 private:
 };
